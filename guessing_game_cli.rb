db43a21eb0e74a9ba6a5d45378caf9421203ffb2
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-16 22:04:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,6 +7,6 @@ def run_guessing_game
   elsif input == "exit"
     puts "Goodbye!"
   else
-    puts "Sorry! The computer guessed #{answer}"
+    puts "Sorry! The computer guessed #{answer}."
   end
 end
